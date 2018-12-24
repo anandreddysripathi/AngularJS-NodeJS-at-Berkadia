d6cc60088ee98591342eb699d342922a88b66999
Sripathi Anand Reddy <44742180+anandreddysripathi@users.noreply.github.com>
2018-12-25 00:27:39 +0530

    Delete hello.rb

--- assignments/week7-NodeJS/hello.rb
+++
@@ -1,7 +0,0 @@
-# Ruby program to count number of even numbers and odd numbers in the array 
-puts("Hi i'm anand");
-array = [1, 2, 4, 2]
-puts("the number of EVEN elements in the array are ");
-puts(array.count { |x| x % 2 == 0 });
-puts("the number of ODD elements in the array are ");
-puts(array.count { |x| x % 2 != 0 });
